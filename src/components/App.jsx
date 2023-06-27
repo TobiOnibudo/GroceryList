61749c10e273220fcffb5a0cca26a4a32d905c21
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2023-06-27 18:42:56 -0300

    enabled changing list feature

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,16 @@ function App() {
     setItems((prevItems) => [...prevItems,inputText])
     setInputText("")
   }
+
+  function deleteItem(id)
+  {
+    setItems((prevItems) => { 
+      return prevItems.filter(
+        (item,index) => {
+            return index != id
+        })
+    })
+  }
   return (
     <div className="container">
       <div className="heading">
@@ -30,7 +40,8 @@ function App() {
       </div>
       <div>
         <ul>
-          {items.map((todoItem) =>  < ToDoItem text = {todoItem}/>)}
+          {items.map((todoItem,index) =>  <ToDoItem key={index} id={index} text = {todoItem} 
+          onChecked = {deleteItem}/>)}
         </ul>
       </div>
     </div>
